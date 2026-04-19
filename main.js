0af1d00ea7d1caec96c2f7ab70a10a62c79aedce
Khainesus <akke2@seznam.cz>
2026-04-19 19:05:43 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,48 @@
 //TODO add imports if needed
-//TODO doc
+import { generateGender, generateName, generateSurname, generateWorkload, generateBirthdate, validateInput } from "./src/employees.js";
+
 /**
- * The main function which calls the application. 
- * Please, add specific description here for the application purpose.
- * @param {object} dtoIn contains count of employees, age limit of employees {min, max}
+ * The main function which generates a list of employees with random data.
+ * @param {object} dtoIn - contains count of employees and age limit of employees
+ * @param {number} dtoIn.count - number of employees to generate
+ * @param {object} dtoIn.age - age range for employees
+ * @param {number} dtoIn.age.min - minimum age of employees
+ * @param {number} dtoIn.age.max - maximum age of employees
  * @returns {Array} of employees
  */
 export function main(dtoIn) {
-  //TODO code
-  //let dtoOut = exMain(dtoIn);
-  return dtoOut;
-}
 
+  // Calling the input validation function
+  validateInput(dtoIn);
+  
+  // Empty array to store generated employees
+  const employees = []; 
+
+  // Loop runs once for each employee that needs to be generated
+  for (let i = 0; i < dtoIn.count; i++) {
+    
+    // Generate random gender
+    const gender = generateGender(); 
+    
+    // Generate name and surname based on gender
+    const name = generateName(gender); 
+    const surname = generateSurname(gender);
+    
+    // Generate random workload 
+    const workload = generateWorkload();
+    
+    // Generate random birthdate within the given age range
+    const birthdate = generateBirthdate(dtoIn.age.min, dtoIn.age.max);
+
+    // Create employee object and add it to the array
+    employees.push({
+      gender: gender,
+      name: name,
+      surname: surname,
+      birthdate: birthdate,
+      workload: workload,
+    });
+  }
+  // Return the complete list of employees
+  return employees;
+}
